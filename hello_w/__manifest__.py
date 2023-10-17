e93d7b29158571ba94b6fea78e0c95fa9b57d9db
admin <hernandezgarciadaniel837@gmail.com>
2023-10-17 09:35:27 -0400

    Clase 12

---
+++ hello_w/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hello W',
+    'summary': 'Hello W',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/ej_pet_view.xml'
+    ],
+}
