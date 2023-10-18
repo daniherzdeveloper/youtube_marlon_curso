ae98e14239b36277e1411ea06cc7ced9af6a8612
admin <hernandezgarciadaniel837@gmail.com>
2023-10-18 17:43:21 -0400

    Rio 2016

---
+++ olympic_medal_table/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Olympic Medal Table',
+    'summary': 'Olympic Medal Table',
+    'author': 'daniherzdeveloper',
+    'category': 'sports',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/athletes_view.xml'
+    ],
+}
---
+++ olympic_medal_table/models/athletes.py
@@ -0,0 +1,14 @@
+from odoo import models, fields, api
+
+class Athletes(models.Model):
+    _name = 'omt.athletes'
+    _description = 'athletes'
+
+    name = fields.Char('name', required=True)
+    country_id = fields.Many2one('res.country', string='Country')
+    gold = fields.Integer('gold')
+    silver = fields.Integer('silver')
+    bronze = fields.Integer('bronze')
+    active = fields.Boolean('active', default=True)
+    
+    
