b67288d21ccb458995db931425448bad54387aaa
admin <hernandezgarciadaniel837@gmail.com>
2023-10-20 06:45:24 -0400

    Ejercicio 5 de nuevos programadores

---
+++ hello_w_05/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Hello W 5',
+    'summary': 'Hello W 5',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/ej_pet_view.xml',
+        'data/pet_data.xml',
+    ],
+}
---
+++ hello_w_05/models/ej_pet.py
@@ -0,0 +1,21 @@
+from odoo import models, fields, api
+
+class EjPet(models.Model):
+    _name = 'ej.pet'
+    _description = 'ej.pet'
+
+    name = fields.Char('name', required=True)
+    age = fields.Char('age', required=True)
+    color = fields.Char('color', required=True)
+    is_new = fields.Boolean('is_new', default=True)
+
+    pretty_name = fields.Boolean('Pretty Name')
+
+    type = fields.Selection([
+        ('dog', 'Dog'),
+        ('cat', 'Cat'),
+        ('bird', 'Bird'),
+        ('fish', 'Fish'),
+        ('other', 'Other'),
+    ], string='type', default='dog', required=True)
+    
