4bb47324e2db7cbdbdeaab023ae238319b52de57
admin <hernandezgarciadaniel837@gmail.com>
2023-10-21 22:43:54 -0400

    modify invoice report

---
+++ hello_w_07_modify_invoice_report/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hello W 07 Modify Invoice Report',
+    'summary': 'Hello W 07 Modify Invoice Report',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base', 'account'],
+    'data': [
+        'report/invoice_report.xml',
+        'report/report.xml',
+    ],
+}
