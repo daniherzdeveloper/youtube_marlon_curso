02204efbcd10e01017e46e7b9c62d0fd95b42f55
admin <hernandezgarciadaniel837@gmail.com>
2023-10-22 08:47:03 -0400

    modify_sale_report

---
+++ hello_w_08_modify_sale_report/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Hello W 08 Modify Sale Report',
+    'summary': 'Hello W 08 Modify Sale Report',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base', 'sale'],
+    'data': [
+        'report/custom_sale_report.xml',
+    ],
+}
