ca0bf13e503acdc17d2834edf192898468d65caf
admin <hernandezgarciadaniel837@gmail.com>
2023-10-24 15:41:19 -0400

    hello_w_11_1_wizard_from_action le agregue otra dependencia porque odoo me decia que era inconsistente mi modulo

---
+++ hello_w_11_1_wizard_from_action/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hello W 11_1 Wizard From Action',
+    'summary': 'Hello W 11_1 Wizard From Action',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base', 'base_vat', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/modify_product_wzd.xml',
+    ],
+}
