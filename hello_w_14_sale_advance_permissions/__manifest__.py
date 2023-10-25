dc73d53de1051d481468b972c709e356bc192295
admin <hernandezgarciadaniel837@gmail.com>
2023-10-25 13:48:26 -0400

    hello_w_14_sale_advance_permissions

---
+++ hello_w_14_sale_advance_permissions/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Hello W 14 Sale Advance Permissions',
+    'summary': 'Hello W 14 Sale Advance Permissions',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base', 'sale_management'],
+    'data': [
+        'security/security.xml',
+        'views/sale_order_view.xml',
+        'views/product_view.xml',
+    ],
+}
