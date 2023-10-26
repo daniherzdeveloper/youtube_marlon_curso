a5e527737a2ff92d4ec5c0ca3b658ed71b617d49
admin <hernandezgarciadaniel837@gmail.com>
2023-10-26 14:07:17 -0400

    Hello_w_16_export_product_excel

---
+++ hello_w_16_export_product_excel/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hello W 16 Export Product Excel',
+    'summary': 'Hello W 16 Export Product Excel',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base', 'stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/product_export_excel_wzd.xml',
+    ],
+}
