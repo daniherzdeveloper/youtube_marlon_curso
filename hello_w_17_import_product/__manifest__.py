978dbe271dbbc99cbaf150026974c65e64fa3c0c
admin <hernandezgarciadaniel837@gmail.com>
2023-10-28 10:05:48 -0400

    hello_w_17_import_product

---
+++ hello_w_17_import_product/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hello W 17 Import Product',
+    'summary': 'Hello W 17 Import Product',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base', 'stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/import_product_views.xml',
+    ],
+}
