ecc2a0d5a534bda2710b4eef758d1e5d1a125ced
admin <hernandezgarciadaniel837@gmail.com>
2023-10-29 16:40:59 -0400

    hello_w_19_custom_create_invoice Add Validations

---
+++ hello_w_19_custom_create_invoice/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hello W 19 Custom Create Invoice',
+    'summary': 'Hello W 19 Custom Create Invoice',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base','account'],
+    'data': [
+      'security/ir.model.access.csv',
+      'wizard/create_invoice_wzd.xml',
+    ],
+}
