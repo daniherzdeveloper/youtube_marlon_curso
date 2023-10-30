d871836788191941ad07bf944e0ada910ade5097
admin <hernandezgarciadaniel837@gmail.com>
2023-10-30 14:33:41 -0400

    hello_w_20_create_move sustitucion del metodo post() por action_post() de el modelo account.move

---
+++ hello_w_20_create_move/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Hello W 20 Create Move',
+    'summary': 'Hello W 20 Create Move',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base','account','product'],
+    'data': [
+      'views/product_template_view.xml',
+    ],
+}
