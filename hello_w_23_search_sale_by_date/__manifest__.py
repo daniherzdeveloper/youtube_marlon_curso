e9b5f4204d5751de98b07f3747db561615fabdc0
admin <hernandezgarciadaniel837@gmail.com>
2023-10-31 15:26:05 -0400

    hello_w_23_search_sale_by_date

---
+++ hello_w_23_search_sale_by_date/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hello W 23 Search Sale by Date',
+    'summary': 'Hello W 23 Search Sale by Date',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base','sale'],
+    'data': [
+      'security/ir.model.access.csv',
+      'wizard/search_sale_wzd.xml',
+    ],
+}
