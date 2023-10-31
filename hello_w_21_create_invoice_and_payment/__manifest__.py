55d6132c63a4380e51d9933e25ca8da5cfa6d0ef
admin <hernandezgarciadaniel837@gmail.com>
2023-10-31 11:46:25 -0400

    hello_w_21_create_invoice_and_payment

---
+++ hello_w_21_create_invoice_and_payment/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Hello W 21 Create Invoice and Payment',
+    'summary': 'Hello W 21 Create Invoice and Payment',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base','account'],
+    'data': [
+      'security/ir.model.access.csv',
+      'security/security.xml',
+      'data/ir_sequence.xml',
+      'views/create_invoice_view.xml',
+    ],
+}
