e7e7a771e5ca383ef4b367d1448ab18f2d7c5759
admin <hernandezgarciadaniel837@gmail.com>
2023-11-01 10:49:28 -0400

    hello_w_26_one2many_fields_notebook

---
+++ hello_w_26_one2many_fields_and_notebook/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Hello W 26 One2many fields and notebook',
+    'summary': 'Hello W 26 One2many fields and notebook',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'data': [
+      'security/ir.model.access.csv',
+      'data/ir_sequence.xml',
+      'views/main_model_view.xml',
+      'reports/model_report.xml',
+      'reports/reports.xml',
+    ],
+}
