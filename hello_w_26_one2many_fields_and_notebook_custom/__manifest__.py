c87c1b0f63cc142e5f3c3901b708b96f0e0ea20e
admin <hernandezgarciadaniel837@gmail.com>
2023-11-01 14:50:17 -0400

    hello_w_26_one2many_fields_and_notebook_custom Modifique el reporte pdf

---
+++ hello_w_26_one2many_fields_and_notebook_custom/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Hello W 26 One2many fields and notebook custom',
+    'summary': 'Hello W 26 One2many fields and notebook custom',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'data': [
+      'security/ir.model.access.csv',
+      'data/ir_sequence.xml',
+      'views/main_model_view.xml',
+      'reports/model_report.xml',
+      'reports/reports.xml',
+    ],
+}
