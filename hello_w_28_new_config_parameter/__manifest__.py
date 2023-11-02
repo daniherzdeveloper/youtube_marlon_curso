d578ad66b4a160e69e96916ba6aa451f5581f013
admin <hernandezgarciadaniel837@gmail.com>
2023-11-02 15:31:15 -0400

    hello_w_28_new_config_parameter  Perzonalice el modelo create.assistant que esta en el wizard herede el modulo res.config.settings para no repetir el campo record_type

---
+++ hello_w_28_new_config_parameter/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Hello W 28 New Config Parameter',
+    'summary': 'Hello W 28 New Config Parameter',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base', 'sale'],
+    'data': [
+      'security/ir.model.access.csv',
+      'views/res_config_setting_view.xml',
+      'wizard/create_assistant_wzd.xml',
+    ],
+}
