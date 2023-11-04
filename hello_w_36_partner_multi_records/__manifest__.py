8bc5062488de9b25f147da2bda08b9632757cffc
admin <hernandezgarciadaniel837@gmail.com>
2023-11-04 10:47:12 -0400

    hello_w_36_partner_multi_records

---
+++ hello_w_36_partner_multi_records/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Hello W 36 Partner Multi Records',
+    'summary': 'Hello W 36 Partner Multi Records',
+    'author': 'daniherzdeveloper',
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base', 'contacts'],
+    'data': [
+      'security/ir.model.access.csv',
+      'security/security.xml',
+      'views/res_partner_view.xml',
+      'views/res_partner_phone_view.xml',
+      'views/menu_view.xml',
+    ],
+}
